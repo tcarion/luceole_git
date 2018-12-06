d80e0e18cd508412e251fe778da30c5adc2a46ec
tcarion <tristancarion@MacBook-Pro-de-Tristan-2.local>
2018-12-06 18:15:02 +0100

    change luceole folder name to website

---
+++ website/luceole/content/home_test.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <title>Accueil</title>
+    <?php include 'header.php';?>
+  </head>
+
+  <body>
+
+    <div class="container">
+        <?php include 'nav.php';?>
+
+      <div class="jumbotron">
+        <h1>Navbar example</h1>
+        <p>This example is a quick exercise to illustrate how the default, static navbar and fixed to top navbar work. It includes the responsive CSS and HTML, so it also adapts to your viewport and device.</p>
+        <p>
+          <a class="btn btn-lg btn-primary" href="../../components/#navbar" role="button">View navbar docs &raquo;</a>
+        </p>
+      </div>
+    </div> <!-- /container -->
+    <?php include 'footer.php';?>
+  </body>
+</html>
---
+++ website/luceole/content/la_cooperative/carte_id.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Carte d'identité</title>
+    <?php include '../header.php';?>
+  </head>
+  <body>
+    <div class="container">
+      <?php include '../nav.php';?>
+      <?php include '../footer.php';?>
+    </div>
+  </body>
+</html>
