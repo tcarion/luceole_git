e5dc13285f9e1675de562ecbb2b3d98fa42b2303
tcarion <tristancarion@gmail.com>
2019-01-12 20:13:29 +0100

    nav for boot4

--- website/luceole/content/nav.php
+++ website/luceole/content/nav.php
@@ -2,67 +2,55 @@
   <img src="<?php echo constant('ROOT_URL'); ?>luceole/files/luceole-header-logo.jpg" style="border-radius: 5px;height: 75px;">
 </div>
 
-<nav class="navbar navbar-default">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-        <span class="sr-only">Toggle navigation</span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-      </button>
-      <a class="navbar-brand" href="#">Lucéole</a>
-    </div>
-
-    <div id="navbar" class="navbar-collapse collapse">
-      <ul class="nav navbar-nav">
-        <li class="nav-item"><a href="luceole/content/home_test.php" target="_top">Accueil</a></li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" target="_top" role="button" aria-haspopup="true" aria-expanded="false">La Coopérative<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="<?php echo constant('ROOT_URL'); ?>index.php?page=coop_carte_id" target="_top">Carte d'identité</a></li>
-            <li><a href="<?php echo constant('ROOT_URL'); ?>index.php?page=vision_globale" target="_top">Vision Globale</a></li>
-            <li><a href="<?php echo constant('ROOT_URL'); ?>index.php?page=missions" target="_top">Missions</a></li>
-            <li><a href="<?php echo constant('ROOT_URL'); ?>index.php?page=valeurs" target="_top">Valeurs</a></li>
-            <li><a href="<?php echo constant('ROOT_URL'); ?>index.php?page=gouvernance" target="_top">Gouvernance</a></li>
-            <li><a href="<?php echo constant('ROOT_URL'); ?>index.php?page=reseau_luceole" target="_top">Le réseau Lucéole</a></li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" target="_top" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Projets<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-            <li><a href="<?php echo constant('ROOT_URL'); ?>index.php?page=habay-la-neuve" target="_top">Habay-la-Neuve</a></li>
-            <li><a href="<?php echo constant('ROOT_URL'); ?>index.php?page=fauvillers" target="_top">Fauvillers</a></li>
-            <li><a href="<?php echo constant('ROOT_URL'); ?>index.php?page=hose" target="_top">Hose</a></li>
-            <li><a href="<?php echo constant('ROOT_URL'); ?>index.php?page=autres" target="_top">Autres</a></li>
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" data-toggle="dropdown" target="_top" role="button" aria-haspopup="true" aria-expanded="false">Fourniture d'électricité<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" target="_top" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Lettres d'information<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" target="_top" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Devenir Coopérateur<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" target="_top" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">FAQ<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-          </ul>
-        </li>
-        <li class="dropdown">
-          <a href="#" class="dropdown-toggle" target="_top" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Contact<span class="caret"></span></a>
-          <ul class="dropdown-menu">
-          </ul>
-        </li>
-      </ul>
-    </div><!--/.nav-collapse -->
-  </div><!--/.container-fluid -->
+<nav class="navbar rounded navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Accueil</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarText">
+    <ul class="navbar-nav mr-auto">
+      <!-- <li class="nav-item active">
+        <a class="nav-link" href="#">Accueil <span class="sr-only">(current)</span></a>
+      </li> -->
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          La Coopérative
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="<?php echo constant('ROOT_URL'); ?>index.php?page=coop_carte_id" target="_top">Carte d'identité</a>
+          <a class="dropdown-item" href="<?php echo constant('ROOT_URL'); ?>index.php?page=vision_globale" target="_top">Vision Globale</a>
+          <a class="dropdown-item" href="<?php echo constant('ROOT_URL'); ?>index.php?page=missions" target="_top">Missions</a>
+          <a class="dropdown-item" href="<?php echo constant('ROOT_URL'); ?>index.php?page=valeurs" target="_top">Valeurs</a>
+          <a class="dropdown-item" href="<?php echo constant('ROOT_URL'); ?>index.php?page=reseau_luceole" target="_top">Le réseau Lucéole</a>
+          <a class="dropdown-item" href="<?php echo constant('ROOT_URL'); ?>index.php?page=gouvernance" target="_top">Gouvernance</a>
+        </div>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Projets
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="<?php echo constant('ROOT_URL'); ?>index.php?page=habay-la-neuve" target="_top">Habay-la-Neuve</a>
+          <a class="dropdown-item" href="<?php echo constant('ROOT_URL'); ?>index.php?page=fauvillers" target="_top">Fauvillers</a>
+          <a class="dropdown-item" href="<?php echo constant('ROOT_URL'); ?>index.php?page=hose" target="_top">Hose</a>
+          <a class="dropdown-item" href="<?php echo constant('ROOT_URL'); ?>index.php?page=autres" target="_top">Autres</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Fourniture d'électricité</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Lettres d'information</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Devenir Coopérateur</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">FAQ</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Contact</a>
+      </li>
+    </ul>
+  </div>
 </nav>
